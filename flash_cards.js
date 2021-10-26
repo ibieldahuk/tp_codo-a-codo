366c24ceac2b9287438e66fc1bfd8a79a9261004
Ibi Eldahuk <ibieldahuk@gmail.com>
2021-10-26 12:58:52 -0300

    Creación de la página de tarjetas; footer es resposive

---
+++ flash_cards.js
@@ -0,0 +1,12 @@
+
+function colorStuff(){
+    let card1 = document.getElementById('card1');
+    let card2 = document.getElementById('card2');
+    let card3 = document.getElementById('card3');
+    let card4 = document.getElementById('card4');
+
+    card1.style.backgroundColor = '#00ff00';
+    card2.style.backgroundColor = '#ff0000';
+    card3.style.backgroundColor = '#ff0000';
+    card4.style.backgroundColor = '#ff0000';
+}
